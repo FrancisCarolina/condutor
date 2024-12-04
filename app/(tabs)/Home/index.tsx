90949576c5ac7c54c511f8f8512774775fbbce41
Francis Carolina <francis.carolinalovo@gmail.com>
2024-12-04 17:42:22 -0300

    lista de veiculos na home

--- app/(tabs)/Home/index.tsx
+++ app/(tabs)/Home/index.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, StyleSheet, Alert, Button } from "react-native";
+import { View, Text, StyleSheet, Alert, Button, ScrollView } from "react-native";
 import { useNavigate } from "react-router-native";
 import QRCode from "react-native-qrcode-svg";
 import { obterUserId, obterToken, deslogar } from "@/utils/storage";
 import axios from "axios";
 import Constants from 'expo-constants';
+import { RadioButton } from 'react-native-paper';
 
 const API_URL = Constants.expoConfig?.extra?.API_URL;
 
@@ -12,6 +13,8 @@ export default function HomePage() {
     const [userName, setUserName] = useState<string | null>(null);
     const [hashCode, setHashCode] = useState<string | null>(null);
     const [isActive, setIsActive] = useState<boolean | null>(null);
+    const [veiculos, setVeiculos] = useState<any[]>([]);
+    const [selectedVeiculo, setSelectedVeiculo] = useState<number | null>(null); // Estado para armazenar o veículo selecionado
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -34,14 +37,25 @@ export default function HomePage() {
                     }
                 );
 
-                const { nome, ativo } = response.data;
-
+                const { nome, ativo, id } = response.data;
                 setUserName(nome);
+                if (nome) {
+
+                    const responseVeiculos = await axios.get(
+                        `${API_URL}/veiculo/condutor/${id}`,
+                        {
+                            headers: {
+                                "x-access-token": token,
+                            },
+                        }
+                    );
+                    setVeiculos(responseVeiculos.data);
+                }
+
                 setIsActive(ativo);
-                setHashCode("$2a$10$tw2R6xF/6F6ceiunSdlxjOCbIuTM264fQ4/YbJ6Jq.xxHVc9QN/7q");
+
             } catch (error) {
                 console.error("Erro ao buscar informações do usuário:", error);
-                Alert.alert("Erro", "Não foi possível carregar as informações do usuário.");
             }
         };
 
@@ -58,25 +72,64 @@ export default function HomePage() {
         }
     };
 
+    const selecionaVeiculo = (id: number) => {
+        setSelectedVeiculo(id)
+        setHashCode("$2a$10$tw2R6xF/6F6ceiunSdlxjOCbIuTM264fQ4/YbJ6Jq.xxHVc9QN/7q");
+    }
+
     return (
         <View style={styles.container}>
             {userName ? (
                 <>
                     <Text style={styles.welcomeText}>Bem-vindo, {userName}!</Text>
                     {isActive ? (
-                        hashCode && (
-                            <QRCode
-                                value={hashCode}
-                                size={350}
-                                backgroundColor="#ffffff"
-                                color="#000000"
-                            />
-                        )
+                        <>
+                            {hashCode ? (
+                                <QRCode
+                                    value={hashCode}
+                                    size={350}
+                                    backgroundColor="#ffffff"
+                                    color="#000000"
+                                />
+                            ) : (
+                                <>
+                                    {veiculos.length > 0 ? (
+                                        <Text style={styles.infoText}>
+                                            Selecione um veículo para gerar seu código de acesso.
+                                        </Text>
+                                    ) : (
+                                        <Text style={styles.infoText}>
+                                            Cadastre um veículo para gerar seu código de acesso.
+                                        </Text>
+                                    )}
+                                </>
+                            )}
+
+                            {/* Lista de veículos somente se a conta estiver ativa */}
+                            {veiculos.length > 0 && (
+                                <View style={styles.veiculoList}>
+                                    <Text style={styles.veiculoListTitle}>Selecione seu veículo ativo:</Text>
+                                    <ScrollView>
+                                        {veiculos.map((veiculo) => (
+                                            <View key={veiculo.id} style={styles.veiculoItem}>
+                                                <RadioButton
+                                                    value={veiculo.id}
+                                                    status={selectedVeiculo === veiculo.id ? "checked" : "unchecked"}
+                                                    onPress={() => selecionaVeiculo(veiculo.id)} // Atualiza o veículo selecionado
+                                                />
+                                                <Text>{veiculo.modelo} - {veiculo.placa}</Text>
+                                            </View>
+                                        ))}
+                                    </ScrollView>
+                                </View>
+                            )}
+                        </>
                     ) : (
                         <Text style={styles.inactiveText}>
                             Sua conta está inativa. Entre em contato com o administrador para ativá-la.
                         </Text>
                     )}
+
                     <Button title="Sair" onPress={handleLogout} color="#FF0000" />
                 </>
             ) : (
@@ -108,5 +161,25 @@ const styles = StyleSheet.create({
         color: "#FF0000",
         textAlign: "center",
         marginVertical: 20,
+    },
+    infoText: {
+        fontSize: 18,
+        color: "#333",
+        textAlign: "center",
+        marginVertical: 20,
+    },
+    veiculoList: {
+        marginTop: 20,
+        width: "80%",
+    },
+    veiculoListTitle: {
+        fontSize: 18,
+        fontWeight: "bold",
+        marginBottom: 10,
+    },
+    veiculoItem: {
+        flexDirection: "row",
+        alignItems: "center",
+        marginVertical: 5,
     },
 });
